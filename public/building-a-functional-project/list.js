ed6a37840162c6ca6cd29f0ba755552977c1336d
Heisemerie <ejike162@gmail.com>
2025-04-10 00:50:10 +0100

    feat: currying functions

--- public/building-a-functional-project/list.js
+++ public/building-a-functional-project/list.js
@@ -1,7 +1,7 @@
 //There are two things to implement; handling the click event & generating content
 //There is the Object-oriented and Functional approach of creating DOM objects
 //Approaching the problem with object-oriented methods; we use the DOM API to create elements, wrapped in a wrapper API
-//Function composition is the logical equivalent of method chaining and allows you to chain function calls
+//"Function composition" is the logical equivalent of "method chaining" and allows you to chain function calls
 //There are two types of compositions; those that read from right to left using "compose()" and left to right using "pipe()"
 //"compose()" and "pipe()" do not exist in JS, you have to write it yourself or use a third party; Ramda
 
@@ -9,14 +9,30 @@
 
 //function to create HTML elements
 const elem = (tag) => document.createElement(tag);
+const text = (content) => document.createTextNode(content);
 
-const el = R.compose(addClass("bg-light"), addClass("p-2"))(elem("div")); //"p-2" added before "bg-light", because it reads from right to left
+const addClass = R.curry(function (className, element) {
+  element.classList.add(className);
+  return element;
+});
 
-document.body.appendChild(el);
+const append = R.curry(function (node, element) {
+  element.appendChild(node);
+  return element;
+});
 
-function addClass(className) {
-  return function (element) {
-    element.classList.add(className);
-    return element;
-  };
+const setAttr = R.curry(function (attributeName, attributeValue, element) {
+  element.setAttribute(attributeName, attributeValue);
+  return element;
+});
+
+function message(content) {
+  return R.compose(
+    append(text(content)),
+    setAttr("data-message", content),
+    addClass("bg-light"),
+    addClass("p-2")
+  )(elem("div")); //"p-2" added before "bg-light", because it reads from right to left
 }
+
+document.body.appendChild(message("hello"));
